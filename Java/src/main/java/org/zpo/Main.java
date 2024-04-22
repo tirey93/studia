509044569ed707653e88c36885d18cecb6665d49
Damian Opolski <tirey93@gmail.com>
2024-04-22 22:36:06 +0200

    added tests

--- Java/src/main/java/org/zpo/Main.java
+++ Java/src/main/java/org/zpo/Main.java
@@ -14,4 +14,8 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
+
+    public static void test(){
+
+    }
 }
---
+++ Java/src/test/java/org/zpo/MainTest.java
@@ -0,0 +1,11 @@
+package org.zpo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void test1() {
+
+    }
+}
