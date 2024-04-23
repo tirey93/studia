d0047f0fdc3945a67b33ed6a310f92377e5038c8
Damian Opolski <tirey93@gmail.com>
2024-04-23 14:41:03 +0200

    finished pesel checker

--- Java/src/main/java/org/zpo/a_pesel/Main.java
+++ Java/src/main/java/org/zpo/a_pesel/Main.java
@@ -1,21 +1,24 @@
 package org.zpo.a_pesel;
 
+import org.zpo.a_pesel.exceptions.PeselException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try{
+            PeselChecker checker = new PeselChecker(args[0]);
+            if(checker.checkSum()){
+                System.out.println("Płeć to: " + checker.getGender());
+                System.out.println("Data urodzenia to: " + checker.getDate());
+                System.out.println("Pesel jest poprawny.");
+            }
+            else {
+                System.out.println("Pesel jest nie poprawny.");
+            }
         }
-    }
-
-    public static void test(){
-
+        catch (PeselException e){
+            System.out.println("Błąd: " + e.getMessage());
+        }
     }
 }
