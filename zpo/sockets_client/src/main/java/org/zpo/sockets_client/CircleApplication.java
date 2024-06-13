04c5363653651a62d34aa61cd630048fe8b9b627
tirey93 <tirey93@gmail.com>
2024-06-13 21:02:18 +0200

    przesunieto okno

--- zpo/sockets_client/src/main/java/org/zpo/sockets_client/CircleApplication.java
+++ zpo/sockets_client/src/main/java/org/zpo/sockets_client/CircleApplication.java
@@ -2,7 +2,9 @@ package org.zpo.sockets_client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +17,9 @@ public class CircleApplication extends Application {
         stage.setTitle("Kółko - Klient");
         stage.setResizable(false);
         stage.setScene(scene);
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        stage.setX(((primScreenBounds.getWidth() - scene.getWidth()) / 2) - 320);
+        stage.setY(((primScreenBounds.getHeight() - scene.getHeight()) / 2));
         stage.show();
     }
 
