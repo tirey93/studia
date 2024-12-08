a756f06eeda4eddba4ccde61b4e827e51f91de74
tirey93 <tirey93@gmail.com>
2024-12-08 17:15:20 +0100

    PSI: po lab1, nie dziala

--- psi/lab1.py
+++ psi/lab1.py
@@ -5,7 +5,7 @@ import numpy as np
 print()
 print()
 
-eta = 0.01 #wspolczynnik uczenia
+eta = 0.01 #wspolczynnik uczenia w drugim zadaniu
 x = np.array([
     [-8, 20],
     [-9, 10],
@@ -22,16 +22,51 @@ x = np.array([
 t = np.array([
     1, 1, 1, 1, 1,  0, 0, 0, 0, 0
 ])
-w = np.random.uniform(low=0.0, high=0.5, size=( 2))
+w = np.random.uniform(low=0.0, high=0.5, size=(x.shape[0], 2))
+b = np.random.uniform(low=0.0, high=0.5, size=(x.shape[0]))
 
-# print(perceptron.output(4, 3, 5, 4 , 0))
-# print(perceptron.output_matrix(w, x, 0))
-
-print("-----------------")
-
-
+print("----WEKTORY WEJSCIOWE-------------")
 print(f"x={x}")
 print(f"t={t}")
 print(f"w={w}")
+print(f"b={b}")
 
-# print(perceptron.method())
+print("-----------------------")
+
+i = 0
+for xi in x:
+    yi = perceptron.output(w, xi, b)
+    ei = t[i] - yi
+
+    if ei == 0:
+        print("ei == 0")
+        pass
+    else:
+        w = w + ei * np.array([xi[0], xi[1]])
+        b = b + ei
+        print(f"e1= {ei}")
+        print(f"w= {w}")
+        print(f"b= {b}")
+
+    print()
+        
+
+
+    i = i + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
