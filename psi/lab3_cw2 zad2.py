ce36e99d4d2b96cb3c8cd0ba9d3629bda08bc6a0
tirey93 <tirey93@gmail.com>
2025-01-27 13:41:39 +0100

    zad 2_2

--- psi/lab3_cw2 zad2.py
+++ psi/lab3_cw2 zad2.py
@@ -1,31 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-y = f(w1:w6) = w1x1 + w2x2 + w3x3 + w4x4 + w5x5 + w6x6
-where (x1,x2,x3,x4,x5,x6)=(4,-2,3.5,5,-11,-4.7) and y=44
-"""
-import pygad
-import numpy
-function_inputs = [4,-2,3.5,5,-11,-4.7]
-desired_output = 44
-def fitness_func(ga_instance, solution, solution_idx):
-    output = numpy.sum(solution*function_inputs)
-    fitness = 1.0 / numpy.abs(output - desired_output)
 
-    return fitness
+import pygad
+import math
+
+def rastring(x, y):
+  return 20 + x**2 + y**2 - 10*(math.cos(2*math.pi*x) + math.cos(2*math.pi*y))
+
+lower_bound = -30
+upper_bound = 30
+
+optimization_type = 'max' 
+def fitness_func(ga_instance, solution, solution_idx):
+    x, y = solution
+    output = rastring(x, y)
+
+    if optimization_type == 'min':
+      return 1.0 / (output + 1e-6)
+    elif optimization_type == 'max':
+      return output
 
 fitness_function = fitness_func
 num_generations = 500
 num_parents_mating = 4
 sol_per_pop = 8
-num_genes = len(function_inputs)
-init_range_low = -2
-init_range_high = 5
+num_genes = 2
+init_range_low = lower_bound
+init_range_high = upper_bound
 parent_selection_type = "sss"
 keep_parents = 1
-#crossover_type = "single_point"
 crossover_type = "uniform"
 mutation_type = "random"
 mutation_percent_genes = 0.01
+
 ga_instance = pygad.GA(num_generations=num_generations,
     num_parents_mating=num_parents_mating,
     fitness_func=fitness_function,
@@ -38,26 +43,28 @@ ga_instance = pygad.GA(num_generations=num_generations,
     crossover_type=crossover_type,
     mutation_type=mutation_type,
     mutation_percent_genes=mutation_percent_genes)
+
 ga_instance.run()
+
 solution, solution_fitness, solution_idx = ga_instance.best_solution()
 print("Parameters of the best solution : {solution}".format(solution=solution))
 print("Fitness value of the best solution ={solution_fitness}".format(solution_fitness=solution_fitness))
-prediction = numpy.sum(numpy.array(function_inputs)*solution)
+prediction = rastring(solution[0],solution[1])
 print("Predicted output based on the best solution : {prediction}".format(prediction=prediction))
+
 ga_instance.plot_fitness()
-# Returning the details of the best solution.
+
 solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
 print(f"Parameters of the best solution : {solution}")
 print(f"Fitness value of the best solution = {solution_fitness}")
 print(f"Index of the best solution : {solution_idx}")
-prediction = numpy.sum(numpy.array(function_inputs)*solution)
+prediction = rastring(solution[0],solution[1])
 print(f"Predicted output based on the best solution : {prediction}")
 
 if ga_instance.best_solution_generation != -1:
     print(f"Best fitness value reached after {ga_instance.best_solution_generation} generations.")
-# Saving the GA instance.
-filename = 'genetic' # The filename to which the instance is saved. The name is without extension.
+
+filename = 'genetic'
 ga_instance.save(filename=filename)
-# Loading the saved GA instance.
 loaded_ga_instance = pygad.load(filename=filename)
 loaded_ga_instance.plot_fitness()
